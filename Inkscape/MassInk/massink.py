fdb5d92575381f13352bcfa0e6375d5a1561b69e
TroiGDev <zapis.troi@gmail.com>
2026-01-25 13:46:11 +0100

    Tried making the inkscape extension work
    The inkscape extension inkex + python system is so bad i cant even get acces to the input field parameters so theres no way i can continue working on this without losing my mind

--- Inkscape/MassInk/massink.py
+++ Inkscape/MassInk/massink.py
@@ -1,4 +1,4 @@
-import inkex
+"""import inkex
 
 class MassInk(inkex.EffectExtension):
     def effect(self):
@@ -6,7 +6,7 @@ class MassInk(inkex.EffectExtension):
 
 if __name__ == "__main__":
     MassInk().run()
-
+"""
 """import inkex
 
 class MassInk(inkex.Effect):
@@ -14,7 +14,7 @@ class MassInk(inkex.Effect):
     def effect(self):
         sel = list(self.svg.selection.values())
 
-        inkex.utils.debug(f"Selected count: {len(sel)}")
+        inkex.utils.debug(self.options.strings)
 
         if sel:
             inkex.utils.debug(f"Selected type: {type(sel[0])}")
@@ -24,3 +24,19 @@ class MassInk(inkex.Effect):
 
 if __name__ == "__main__":
     MassInk().run()"""
+
+
+import inkex
+
+class MassInk(inkex.Effect):
+    def effect(self):
+        strings = self.args.get('inputstrings', '')       #example : "new game||options||exit to|desktop"
+
+        #split correclty
+        lines = strings.split("||")
+
+        for i in range(len(lines)):
+            inkex.utils.debug(lines[i])
+        
+if __name__ == "__main__":
+    MassInk().run()
